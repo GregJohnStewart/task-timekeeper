55981cbddab24362c3a6dad88b4f3b3cd105bd90
Greg J Stewart <contact@gjstewart.net>
2019-03-31 20:27:58 -0400

    Initial dump of setup of initial code skeleton

---
+++ BaseCode/src/main/java/com/gjs/taskTimekeeper/backend/TimeManager.java
@@ -0,0 +1,13 @@
+package com.gjs.taskTimekeeper.backend;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.LinkedHashMap;
+
+public class TimeManager {
+	private static final Logger LOGGER = LoggerFactory.getLogger(TimeManager.class);
+
+	private LinkedHashMap<Object, Object> workTimespans = null;
+}
---
+++ BaseCode/src/main/java/com/gjs/taskTimekeeper/backend/Timespan.java
@@ -0,0 +1,8 @@
+package com.gjs.taskTimekeeper.backend;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Timespan {
+	private static final Logger LOGGER = LoggerFactory.getLogger(Timespan.class);
+}
