6034ed85f8146466f2915d1d9c3ff242fa76c38d
Greg Stewart <contact@gjstewart.net>
2019-04-12 12:57:05 -0400

    Hammered out handling of default config, as well as that from environment.

--- DesktopApp/src/main/java/com/gjs/taskTimekeeper/desktopApp/Main.java
+++ DesktopApp/src/main/java/com/gjs/taskTimekeeper/desktopApp/Main.java
@@ -1,7 +1,14 @@
 package com.gjs.taskTimekeeper.desktopApp;
 
-public class Main {
-	public static void main(String[] args){
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+public class Main {
+	private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
+	public static void main(String[] args){
+		LOGGER.info("Starting run of TaskTimekeeper.");
+		Configuration.readCmdLinesIn(args);
+		Configuration.logOutProperties();
 	}
 }
