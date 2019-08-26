a67f1432f7f1f9c035f2cabc14dbded00149ccc6
Greg Stewart <contact@gjstewart.net>
2019-08-26 17:01:03 -0400

    Moved to using an svg as main logo image.

--- logo/generate-logos.sh
+++ logo/generate-logos.sh
@@ -2,13 +2,17 @@
 # This script automatically generates the logos needed for the project
 # Must have inkscape installed
 
-mainFile="logo-main.png";
-destinationDir="generated/desktop-app/";
+mainFile="logo-main.svg";
+destinationDir="generated/";
+
+# Generate logo-main png from the main svg
+mainSize="1000"
+inkscape -z -e ${destinationDir}logo-main.png -w $mainSize -h $mainSize $mainFile
 
 # Generate the gui icon. Used as the icon for the Desktop GUI and taskbar icon.
 guiIconSize="128"
-inkscape -z -e ${destinationDir}gui-icon.png -w $guiIconSize -h $guiIconSize $mainFile
+inkscape -z -e ${destinationDir}desktop-app/gui-icon.png -w $guiIconSize -h $guiIconSize $mainFile
 
 # generate the desktop icon, or the icon of the executable when installed.
 desktopIconSize="500"
-inkscape -z -e ${destinationDir}desktop-icon.icn -w $desktopIconSize -h $desktopIconSize $mainFile
+inkscape -z -e ${destinationDir}desktop-app/desktop-icon.icn -w $desktopIconSize -h $desktopIconSize $mainFile
