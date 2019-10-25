8111da88659068921a774c123a0e18ac4f8edc0d
Greg Stewart <contact@gjstewart.net>
2019-10-25 16:37:46 -0400

    Made gui runner test use existing dataset to test with.

--- DesktopApp/src/test/java/com/gjs/taskTimekeeper/desktopApp/runner/GuiRunnerTest.java
+++ DesktopApp/src/test/java/com/gjs/taskTimekeeper/desktopApp/runner/GuiRunnerTest.java
@@ -1,15 +1,25 @@
 package com.gjs.taskTimekeeper.desktopApp.runner;
 
+import com.gjs.taskTimekeeper.baseCode.managerIO.dataSource.FileDataSourceTest;
+import com.gjs.taskTimekeeper.desktopApp.config.ConfigKeys;
 import com.gjs.taskTimekeeper.desktopApp.config.Configuration;
 import com.gjs.taskTimekeeper.desktopApp.managerIO.LocalFile;
+import java.io.File;
 import org.junit.Before;
 import org.junit.Test;
 import org.kohsuke.args4j.CmdLineException;
 
 public class GuiRunnerTest {
+    private final File testFile =
+            new File(
+                    FileDataSourceTest.class
+                            .getClassLoader()
+                            .getResource("testTimeManagerData/fully_populated.json")
+                            .getFile());
 
     @Before
     public void setup() {
+        Configuration.setFile(ConfigKeys.SAVE_FILE, testFile);
         try {
             Configuration.finalizeConfig();
             LocalFile.ensureFilesExistWritable();
@@ -18,7 +28,6 @@ public class GuiRunnerTest {
         }
     }
 
-    // TODO:: make this use a predefined dataset
     @Test
     public void runGui() {
         // figure out how to close automatically for testing
