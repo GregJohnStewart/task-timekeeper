554b71abde09e798c6af30a31fe024f92307302a
Greg Stewart <contact@gjstewart.net>
2019-11-04 09:10:09 -0500

    Added idea of a "savable" config entry for saving configuration values

--- DesktopApp/src/main/java/com/gjs/taskTimekeeper/desktopApp/config/ConfigKeys.java
+++ DesktopApp/src/main/java/com/gjs/taskTimekeeper/desktopApp/config/ConfigKeys.java
@@ -7,36 +7,38 @@ import java.util.List;
 /** Keys for accessing configuration. */
 public enum ConfigKeys {
     // basic config keys, from project.properties
-    APP_NAME("app.name"),
-    APP_VERSION("app.version"),
-    APP_BUILDTIME("app.buildtime"),
-    LIB_VERSION("lib.version"),
+    APP_NAME(false, "app.name"),
+    APP_VERSION(false, "app.version"),
+    APP_BUILDTIME(false, "app.buildtime"),
+    LIB_VERSION(false, "lib.version"),
     // Other config
-    CONFIG_FILE("configFile", "TKPR_CONFIG_FILE", true, false),
-    UI_OPTIONS_FILE("uiOptionsFile", "TKPR_UI_CONFIG_FILE", true, false),
-    SAVE_FILE("saveFile", "TKPR_SAVE_FILE", true, true),
-    RUN_MODE("run.mode", "TKPR_MODE"),
+    CONFIG_FILE(false, "configFile", "TKPR_CONFIG_FILE", true, false),
+    UI_OPTIONS_FILE(true, "uiOptionsFile", "TKPR_UI_CONFIG_FILE", true, false),
+    SAVE_FILE(true, "saveFile", "TKPR_SAVE_FILE", true, true),
+    RUN_MODE(true, "run.mode", "TKPR_MODE"),
     // Running config, for single mode
-    SINGLE_MODE_HELP("consoleHelp"),
+    SINGLE_MODE_HELP(false, "consoleHelp"),
     // default values
-    DEFAULT_CONFIG_FILE("default.configFile", CONFIG_FILE),
-    DEFAULT_UI_OPTIONS_FILE("default.uiOptionsFile", UI_OPTIONS_FILE),
-    DEFAULT_SAVE_FILE("default.saveFile", SAVE_FILE),
-    DEFAULT_RUN_MODE("default.runMode", RUN_MODE),
+    DEFAULT_CONFIG_FILE(false, "default.configFile", CONFIG_FILE),
+    DEFAULT_UI_OPTIONS_FILE(false, "default.uiOptionsFile", UI_OPTIONS_FILE),
+    DEFAULT_SAVE_FILE(false, "default.saveFile", SAVE_FILE),
+    DEFAULT_RUN_MODE(false, "default.runMode", RUN_MODE),
     // static files
-    STATIC_GUI_ICON("static.guiIconFile"),
+    STATIC_GUI_ICON(false, "static.guiIconFile"),
     // other
-    GITHUB_REPO_URL("urls.github"),
-    GITHUB_DESKTOP_APP_README("urls.desktopReadme"),
-    DONATE_URL("urls.donateUrl");
+    GITHUB_REPO_URL(false, "urls.github"),
+    GITHUB_DESKTOP_APP_README(false, "urls.desktopReadme"),
+    DONATE_URL(false, "urls.donateUrl");
 
+    public final boolean savable;
     public final String key;
     public final String envVar;
     public final ConfigKeys defaultFor;
     public final boolean isFile;
     public final boolean needsFile;
 
-    ConfigKeys(String key) {
+    ConfigKeys(boolean savable, String key) {
+        this.savable = savable;
         this.key = key;
         this.envVar = null;
         this.defaultFor = null;
@@ -44,7 +46,8 @@ public enum ConfigKeys {
         this.needsFile = false;
     }
 
-    ConfigKeys(String key, String envVar) {
+    ConfigKeys(boolean savable, String key, String envVar) {
+        this.savable = savable;
         this.key = key;
         this.envVar = envVar;
         this.defaultFor = null;
@@ -52,7 +55,8 @@ public enum ConfigKeys {
         this.needsFile = false;
     }
 
-    ConfigKeys(String key, String envVar, boolean isFile, boolean needsFile) {
+    ConfigKeys(boolean savable, String key, String envVar, boolean isFile, boolean needsFile) {
+        this.savable = savable;
         this.key = key;
         this.envVar = envVar;
         this.defaultFor = null;
@@ -60,7 +64,8 @@ public enum ConfigKeys {
         this.needsFile = needsFile;
     }
 
-    ConfigKeys(String key, ConfigKeys defaultFor) {
+    ConfigKeys(boolean savable, String key, ConfigKeys defaultFor) {
+        this.savable = savable;
         this.key = key;
         this.envVar = null;
         this.defaultFor = defaultFor;
@@ -92,6 +97,16 @@ public enum ConfigKeys {
         List<ConfigKeys> keys = new ArrayList<>();
         for (ConfigKeys key : ConfigKeys.values()) {
             if (key.isFile) {
+                keys.add(key);
+            }
+        }
+        return keys;
+    }
+
+    public static Collection<ConfigKeys> getKeysThatAreSavable() {
+        List<ConfigKeys> keys = new ArrayList<>();
+        for (ConfigKeys key : ConfigKeys.values()) {
+            if (key.savable) {
                 keys.add(key);
             }
         }
