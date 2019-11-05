86b30ada9207fc5a37381c01b6884964337208e7
Greg Stewart <contact@gjstewart.net>
2019-11-05 17:00:21 -0500

    dumb test of RunMode for coverage fix

--- DesktopApp/src/test/java/com/gjs/taskTimekeeper/desktopApp/config/RunModeTest.java
+++ DesktopApp/src/test/java/com/gjs/taskTimekeeper/desktopApp/config/RunModeTest.java
@@ -2,10 +2,9 @@ package com.gjs.taskTimekeeper.desktopApp.config;
 
 import static org.junit.Assert.*;
 
-import com.gjs.taskTimekeeper.baseCode.managerIO.dataSource.DataSourceTest;
 import org.junit.Test;
 
-public class RunModeTest extends DataSourceTest {
+public class RunModeTest {
     @Test
     public void test() {
         RunMode.values();
