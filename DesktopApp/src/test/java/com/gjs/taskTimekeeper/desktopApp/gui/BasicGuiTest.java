8b933457af0eaf46127a4010b1c309fef6723114
Greg Stewart <contact@gjstewart.net>
2020-02-28 22:23:41 -0500

    Better ways of interacting with frames and buttons

--- DesktopApp/src/test/java/com/gjs/taskTimekeeper/desktopApp/gui/BasicGuiTest.java
+++ DesktopApp/src/test/java/com/gjs/taskTimekeeper/desktopApp/gui/BasicGuiTest.java
@@ -1,13 +1,13 @@
 package com.gjs.taskTimekeeper.desktopApp.gui;
 
 import com.gjs.taskTimekeeper.desktopApp.gui.utils.GuiNavigation;
-import org.assertj.swing.core.GenericTypeMatcher;
+import com.gjs.taskTimekeeper.desktopApp.gui.utils.matcher.ButtonTextMatcher;
+import com.gjs.taskTimekeeper.desktopApp.gui.utils.matcher.InternalFrameMatcher;
+import org.assertj.swing.fixture.JButtonFixture;
 import org.assertj.swing.fixture.JInternalFrameFixture;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.swing.*;
 
 public class BasicGuiTest extends GuiTest {
     private static final Logger LOGGER = LoggerFactory.getLogger(BasicGuiTest.class);
@@ -30,13 +30,20 @@ public class BasicGuiTest extends GuiTest {
         GuiNavigation.clickMenuInfo(this.fixture);
         GuiNavigation.clickMenuItem(this.fixture, "aboutMenuItem");
 
-        JInternalFrameFixture frame = this.fixture.internalFrame(new GenericTypeMatcher<JInternalFrame>(JInternalFrame.class) {
-            @Override
-            protected boolean isMatching(JInternalFrame component) {
-                return true;
-            }
-        });
+        JInternalFrameFixture frame = this.fixture.internalFrame(new InternalFrameMatcher());
 
         frame.requireTitle("About Task Timekeeper");
+
+        JButtonFixture jButtonFixture = frame.button(new ButtonTextMatcher("OK"));
+
+        LOGGER.info("Ensuring visible.");
+        frame.requireVisible();
+
+        LOGGER.info("Clicking ok button.");
+        jButtonFixture.click();
+
+        LOGGER.info("Requiring frame closed.");
+
+        frame.requireNotVisible();
     }
 }
---
+++ DesktopApp/src/test/java/com/gjs/taskTimekeeper/desktopApp/gui/utils/matcher/ButtonTextMatcher.java
@@ -0,0 +1,20 @@
+package com.gjs.taskTimekeeper.desktopApp.gui.utils.matcher;
+
+import org.assertj.swing.core.GenericTypeMatcher;
+
+import javax.swing.*;
+
+public class ButtonTextMatcher extends GenericTypeMatcher<JButton> {
+
+    private final String buttonText;
+
+    public ButtonTextMatcher(String buttonText) {
+        super(JButton.class);
+        this.buttonText = buttonText;
+    }
+
+    @Override
+    protected boolean isMatching(JButton component) {
+        return component.getText().equalsIgnoreCase(this.buttonText);
+    }
+}
