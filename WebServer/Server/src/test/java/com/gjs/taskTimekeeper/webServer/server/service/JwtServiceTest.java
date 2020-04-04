4c9e339dcd40afb0d9342e789b6f1697a20e2b26
Greg Stewart <contact@gjstewart.net>
2020-04-03 21:59:12 -0400

    Improved jwt test

--- WebServer/Server/src/test/java/com/gjs/taskTimekeeper/webServer/server/service/JwtServiceTest.java
+++ WebServer/Server/src/test/java/com/gjs/taskTimekeeper/webServer/server/service/JwtServiceTest.java
@@ -1,20 +1,24 @@
 package com.gjs.taskTimekeeper.webServer.server.service;
 
 import com.gjs.taskTimekeeper.webServer.server.mongoEntities.User;
+import com.gjs.taskTimekeeper.webServer.server.mongoEntities.pojo.UserLevel;
 import com.gjs.taskTimekeeper.webServer.server.testResources.RunningServerTest;
 import com.gjs.taskTimekeeper.webServer.server.testResources.TestMongo;
 import com.gjs.taskTimekeeper.webServer.server.testResources.entity.UserUtils;
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.Header;
+import io.restassured.response.ValidatableResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
+import javax.ws.rs.core.Response;
 import java.util.Date;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static io.restassured.RestAssured.given;
 
 @QuarkusTest
 @QuarkusTestResource(TestMongo.class)
@@ -31,19 +35,31 @@ public class JwtServiceTest extends RunningServerTest {
     @BeforeEach
     public void setTestUser(){
         this.testUser = UserUtils.getTestUser(true, passwordService);
+        this.testUser.setLastLogin(new Date());
+        this.testUser.setNumLogins(1L);
+    }
+
+    private void assertProperToken(String token){
+        ValidatableResponse response = given()
+                .when()
+                .header(new Header("Authorization", "Bearer " + token))
+                .get("/api/user/auth/tokenCheck")
+                .then()
+                .statusCode(Response.Status.OK.getStatusCode());
     }
 
 
     @Test
     public void test(){
-        this.testUser.setLastLogin(new Date());
-        this.testUser.setNumLogins(1L);
+        this.testUser.setLevel(UserLevel.REGULAR);
 
         String jwt = jwtService.generateTokenString(this.testUser, false);
 
         LOGGER.info("Created test user's jwt: {}", jwt);
 
-        assertNotNull(jwt);
+        this.assertProperToken(jwt);
     }
 
+    //TODO:: check for expiration
+    //TODO:: check for user level
 }
