6d9a2b08fe99e6ca0266681b3bfb80474ded20d6
Greg Stewart <contact@gjstewart.net>
2020-05-08 00:11:17 -0400

    Added spinner capabilities

--- WebServer/Server/src/main/resources/META-INF/resources/res/script/main.js
+++ WebServer/Server/src/main/resources/META-INF/resources/res/script/main.js
@@ -1,5 +1,27 @@
 var loginToken = Cookies.get("loginToken");
 
+var spinnerOpts = {
+  lines: 13, // The number of lines to draw
+  length: 38, // The length of each line
+  width: 17, // The line thickness
+  radius: 45, // The radius of the inner circle
+  scale: 1, // Scales overall size of the spinner
+  corners: 1, // Corner roundness (0..1)
+  color: '#000000', // CSS color or array of colors
+  fadeColor: 'transparent', // CSS color or array of colors
+  speed: 1, // Rounds per second
+  rotate: 0, // The rotation offset
+  animation: 'spinner-line-shrink', // The CSS animation name for the lines
+  direction: 1, // 1: clockwise, -1: counterclockwise
+  zIndex: 2e9, // The z-index (defaults to 2000000000)
+  className: 'spinner', // The CSS class to assign to the spinner
+  top: '50%', // Top position relative to parent
+  left: '50%', // Left position relative to parent
+  shadow: '0 0 1px transparent', // Box-shadow for the lines
+  position: 'absolute' // Element positioning
+};
+
+
 function userLoggedIn(){
     //TODO:: if cookie not null, do token check call
     return loginToken != null;
@@ -46,6 +68,16 @@ function getServerStatus(){
         output += "</ul>";
         serverStatus.attr("data-content", output);
     });
+}
+
+
+function doLogin(event){
+    console.log("Attempting to log user in...");
+    event.preventDefault();
+
+    var spinner = new Spinner(spinnerOpts).spin(document.body);
+
+    return true;
 }
 
 $(document).ready(function() {
